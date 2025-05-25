45bbecd1639642ae71d68e39b9ef7c480c767603
Gknowsss <geecostan@gmail.com>
2025-05-25 14:49:41 -0400

    Testing cURL queries in php for API

---
+++ live.php
@@ -0,0 +1,32 @@
+<html>
+
+<head></head>
+
+<body>
+
+    <?php
+        $curl = curl_init();
+            
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/5UXWX7C5*BA?format=xml&modelyear=2011',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer YOUR_API_KEY'
+            ),
+        ));
+            
+        $response = curl_exec($curl);
+            
+        curl_close($curl);
+        echo $response;
+    ?>
+
+</body>
+
+</html>
